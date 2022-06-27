98b9afd5b175dd2fad28d2e89c419d2555e1adc0
reinardricky <pascalis.r.interns@aux.gojek.com>
2022-06-27 16:39:06 +0700

    add feature open and close navBar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import LogoRr from '../public/assets/logo/Rr.png';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { useState } from 'react';
 
 export const NavBar = () => {
+	const [nav, setNav] = useState(false);
+
+	const handleNav = () => {
+		setNav(!nav);
+	};
+
 	return (
 		<div className="fixed w-full h-20 shadow-xl z-[100] bg-[#a6defd]">
 			<div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -35,16 +43,28 @@ export const NavBar = () => {
 							</li>
 						</Link>
 					</ul>
-					<div className="md:hidden">
+					<div className="md:hidden" onClick={handleNav}>
 						<AiOutlineMenu size={30} />
 					</div>
 				</div>
 			</div>
-			<div className="fixed left-0 top-0 w-full h-screen bg-black/70">
-				<div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#a6defd] p-10 ease-in duration-500 ">
+			<div
+				className={
+					nav
+						? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70 p-10 ease-linear duration-300'
+						: ''
+				}
+			>
+				<div
+					className={
+						nav
+							? 'fixed left-0 top-0 w-[75%] sm:w-[60%] h-screen bg-[#a6defd] p-10 ease-in duration-300 '
+							: 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+					}
+				>
 					<div>
 						<div className="flex w-full items-center justify-center">
-							<Image src={LogoRr} alt="logo Rr." width="80" height="80" />
+							<Image src={LogoRr} alt="/" width="80" height="80" />
 						</div>
 					</div>
 					<div className="py-4 flex flex-col text-center">
@@ -65,13 +85,30 @@ export const NavBar = () => {
 								<li className="py-4 text-sm">Contact</li>
 							</Link>
 						</ul>
-						<div className="pt-10">
+						<div className="pt-20">
 							<p className="uppercase tracking-widest text-[#fd4557]">
 								Let's Connect
 							</p>
+							<div className="flex items-center justify-center my-4 w-full px-2 text-2xl">
+								<div className="rounded-full  p-3 cursor-pointer hover:scale-150 ease-in duration-300">
+									<FaLinkedin />
+								</div>
+								<div className="rounded-full  p-3 cursor-pointer hover:scale-150 ease-in duration-300">
+									<FaGithub />
+								</div>
+								<div className="rounded-full  p-3 cursor-pointer hover:scale-150 ease-in duration-300">
+									<AiOutlineMail />
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
+				<div
+					className={
+						nav ? 'fixed right-0 top-0 w-[25%] sm:w-[40%] h-screen ' : ''
+					}
+					onClick={handleNav}
+				></div>
 			</div>
 		</div>
 	);
