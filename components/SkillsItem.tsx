5db043587aacb612bb2c1e07532507eddde494bd
reinardricky <pascalis.r.interns@aux.gojek.com>
2022-06-28 08:52:44 +0700

    change skills in small devices

--- components/SkillsItem.tsx
+++ components/SkillsItem.tsx
@@ -8,7 +8,7 @@ type SkillsItemType = {
 const SkillsItem = ({ logo, name }: SkillsItemType) => {
 	return (
 		<div className="p-6 shadow-md shadow-[#fd4557] rounded-xl hover:scale-105 ease-in duration-300 bg-[#a6ebe8]">
-			<div className="grid grid-cols-2 gap-4 justify-center items-center ">
+			<div className="grid  sm:grid-cols-2 gap-4 justify-center items-center ">
 				<div className="m-auto">
 					<Image src={logo} alt="/" width="64px" height="64px" />
 				</div>
