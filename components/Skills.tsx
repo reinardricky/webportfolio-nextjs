ed03fe1a583f6367fda3062986fd4c68a96b0e1e
reinardricky <pascalis.r.interns@aux.gojek.com>
2022-06-28 14:38:43 +0700

    Change set of skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,13 +1,11 @@
 import Flutter from '../public/assets/skills/flutter.png';
-import HTML from '../public/assets/skills/html.png';
-import CSS from '../public/assets/skills/css.png';
-import Javascript from '../public/assets/skills/javascript.png';
 import React from '../public/assets/skills/react.png';
 import NextJs from '../public/assets/skills/nextjs.png';
 import Node from '../public/assets/skills/node.png';
 import Tailwind from '../public/assets/skills/tailwind.png';
-import Git from '../public/assets/skills/github1.png';
+import Git from '../public/assets/skills/git.png';
 import SQL from '../public/assets/skills/sql.png';
+import Typescript from '../public/assets/skills/typescript.svg';
 import SkillsItem from './SkillsItem';
 
 const Skills = () => {
@@ -16,15 +14,13 @@ const Skills = () => {
 			<div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
 				<h2 className="tracking-wider py-4">Skills</h2>
 				<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
-					<SkillsItem logo={HTML} name="HTML" />
-					<SkillsItem logo={CSS} name="CSS" />
-					<SkillsItem logo={Javascript} name="Javascript" />
-					<SkillsItem logo={React} name="React" />
-					<SkillsItem logo={NextJs} name="Next" />
+					<SkillsItem logo={Typescript} name="Typescript" />
+					<SkillsItem logo={React} name="React.js" />
+					<SkillsItem logo={NextJs} name="Next.js" />
 					<SkillsItem logo={Flutter} name="Flutter" />
-					<SkillsItem logo={Node} name="Node" />
+					<SkillsItem logo={Node} name="Node.js" />
 					<SkillsItem logo={Tailwind} name="Tailwind" />
-					<SkillsItem logo={Git} name="Github" />
+					<SkillsItem logo={Git} name="Git" />
 					<SkillsItem logo={SQL} name="SQL" />
 				</div>
 			</div>
--- components/SkillsItem.tsx
+++ components/SkillsItem.tsx
@@ -7,7 +7,7 @@ type SkillsItemType = {
 
 const SkillsItem = ({ logo, name }: SkillsItemType) => {
 	return (
-		<div className="p-4 sm:p-6 shadow-md shadow-[#fd4557] rounded-xl hover:scale-105 ease-in duration-300 bg-[#a6ebe8]">
+		<div className="p-4 sm:p-6 shadow-md shadow-[#fd4557] rounded-xl bg-[#a6ebe8]">
 			<div className="grid sm:grid-cols-2 gap-2 sm:gap-4 justify-center items-center ">
 				<div className="m-auto w-[32px] h-[32px] sm:w-[64px] sm:h-[64px] relative">
 					<Image src={logo} alt="/" layout="fill" />
