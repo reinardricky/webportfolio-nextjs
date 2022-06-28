ad403222013a27487344f9077a23c04e9bdb51f9
reinardricky <pascalis.r.interns@aux.gojek.com>
2022-06-28 07:35:49 +0700

    test update meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,16 @@ const Home: NextPage = () => {
 					name="description"
 					content="Website Portfolio of Pascalis Reinard Rickyputra or Reinardricky"
 				/>
+				<meta charSet="UTF-8" />
+				<meta
+					name="keywords"
+					content="HTML, CSS, JavaScript, Reinardricky, reinardricky, web portfolio, Pascalis Reinard Rickyputra"
+				/>
+				<meta name="author" content="Pascalis Reinard Rickyputra" />
+				<meta
+					property="og:image"
+					content="../public/assets/logo/MetaImage.png"
+				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<HomeScreen />
